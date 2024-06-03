9096c7e370becd31f6b6cb1e835e4c7de624f3f5
Alexey Flyagin <alexeyflyagin06@gmail.com>
2024-06-03 03:06:06 +0200

    Start bot

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import asyncio
 
+from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+
+import handlers
+from config import settings
+
 
 async def main():
-    print("hello")
+    bot_properties = DefaultBotProperties(parse_mode="HTML")
+    bot = Bot(token=settings.BOT_TOKEN, default=bot_properties)
+    db = Dispatcher()
+    db.include_router(handlers.router)
+    await bot.delete_webhook(drop_pending_updates=True)
+    await db.start_polling(bot)
 
 
 if __name__ == '__main__':
